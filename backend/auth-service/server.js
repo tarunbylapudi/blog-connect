167275eafaecf53974a24eea71cb4a475bd3ff98
Tarun Bylapudi <bylasoft@gmail.com>
2023-07-23 00:08:59 +0530

    Added Actuator

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -10,6 +10,7 @@ const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
+const actuator = require('express-actuator');
 
 const DbConnect = require("./config/db");
 const User = require("./model/User");
@@ -61,6 +62,16 @@ app.use(hpp());
 
 //allow cors
 app.use(cors());
+
+const options = {
+  basePath: '/management', // It will set /management/info instead of /info
+  infoGitMode: 'full', // the amount of git information you want to expose, 'simple' or 'full',
+  infoBuildOptions: null, // extra information you want to expose in the build object. Requires an object.
+  infoDateFormat: null, // by default, git.commit.time will show as is defined in git.properties. If infoDateFormat is defined, moment will format git.commit.time. See https://momentjs.com/docs/#/displaying/format/.
+  customEndpoints: [] // array of custom endpoints
+};
+
+app.use(actuator(options));
 
 //routes
 app.use("/api/v1/auth", blogs);
--- backend/blog-service/server.js
+++ backend/blog-service/server.js
@@ -60,19 +60,18 @@ app.use(hpp());
 //allow cors
 app.use(cors());
 
-//routes
-app.use("/api/v1/blogs", blogRoutes);
-
 const options = {
-  basePath: "/management", // It will set /management/info instead of /info
-  infoGitMode: "simple", // the amount of git information you want to expose, 'simple' or 'full',
+  basePath: '/management', // It will set /management/info instead of /info
+  infoGitMode: 'full', // the amount of git information you want to expose, 'simple' or 'full',
   infoBuildOptions: null, // extra information you want to expose in the build object. Requires an object.
   infoDateFormat: null, // by default, git.commit.time will show as is defined in git.properties. If infoDateFormat is defined, moment will format git.commit.time. See https://momentjs.com/docs/#/displaying/format/.
-  customEndpoints: [], // array of custom endpoints
+  customEndpoints: [] // array of custom endpoints
 };
 
-// Configure actuator
 app.use(actuator(options));
+
+//routes
+app.use("/api/v1/blogs", blogRoutes);
 
 //error handler
 app.use(errorHandler);
